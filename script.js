5e9569abade80852deeb40b5d2ee9f141f89e5c2
Notliam99 <95384035+Notliam99@users.noreply.github.com>
2022-09-02 22:46:29 +1200

    improvments
    added js loging and some css stileing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var count = 1;
 document.getElementById('button').onclick = function() {
+   console.log("you have liked monkeys " + count + " times")
    alert("you have liked monkeys " + (count++) + " times");
 };
