0f53074090408437c6f5b449b966cc406d7f013b
Notliam99 <95384035+Notliam99@users.noreply.github.com>
2022-09-03 19:26:57 +1200

    added another button
    starting to rap my head arrond java script bacis

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var count = 1;
+var button1prompt = 'str'
+var button1resault = 'str'
+
 document.getElementById('button').onclick = function() {
    console.log("you have liked monkeys " + count + " times")
    alert("you have liked monkeys " + (count++) + " times");
+};
+document.getElementById('button1').onclick = function() {
+   button1prompt = 'You Have Liked Monkeys ' + (count - 1) + ' Times. In Your Opion Are Monkeys Cool'
+   button1resault = prompt(button1prompt);
+   if (confirm('just to confirm you awnserd ' + button1resault + ' to the question ' + button1prompt ) == true ){
+      console.log('future api post rec')
+   }else{
+      console.log('user cansled input');
+   };
 };
